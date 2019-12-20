e88b0784f76fc3994a3c9e16f49cb56d9acb80d1
Leopc1977 <leophancaowz@gmail.com>
2019-12-20 23:01:54 +0100

    x

--- sources/player.lua
+++ sources/player.lua
@@ -5,7 +5,7 @@ myMap = require ("map")
 myLib = require("lib")
 myGameRessources = require("gameRessources")
 
---METTRE DANS RESSOURCES
+
 player.walk = {}
 player.walk[1] = love.graphics.newImage("images/sprite/Walk (1).png")
 player.x = 100
@@ -14,6 +14,47 @@ player.width = player.walk[1]:getWidth()
 player.height = player.walk[1]:getHeight()
 player.scaleX = 0.15
 player.scaleY = 0.15
+
+gameRessources = {}
+
+gameRessources.toy = {}
+gameRessources.gift = {}
+
+gameRessources.toy.car = {}
+gameRessources.toy.book = {}
+gameRessources.toy.stick = {}
+gameRessources.toy.ball = {}
+gameRessources.toy.deer = {}
+
+gameRessources.toy.car.img = love.graphics.newImage("images/objects/voiture.png")
+
+gameRessources.toy.book.img = love.graphics.newImage("images/objects/livre.png")
+
+gameRessources.toy.stick.img = love.graphics.newImage("images/objects/baton.png")
+
+gameRessources.toy.ball.img = love.graphics.newImage("images/objects/boule.png")
+
+gameRessources.toy.deer.img = love.graphics.newImage("images/objects/cerf.png")
+
+gameRessources.gift.blue = {}
+gameRessources.gift.brown = {}
+gameRessources.gift.green = {}
+gameRessources.gift.pink = {}
+gameRessources.gift.red = {}
+
+gameRessources.gift.blue.img = love.graphics.newImage("images/objects/giftBlue.png")
+
+gameRessources.gift.brown.img = love.graphics.newImage("images/objects//giftBrown.png")
+
+gameRessources.gift.green.img = love.graphics.newImage("images/objects/giftGreen.png")
+
+gameRessources.gift.pink.img = love.graphics.newImage("images/objects/giftPink.png")
+
+gameRessources.gift.red.img = love.graphics.newImage("images/objects/giftRed.png")
+
+gameRessources.scaleX = 2
+gameRessources.scaleY = 2
+
 
 dt = 1/60
 
@@ -137,11 +178,11 @@ function player.draw()
 	end
 
 	if drawCond.toy == true then
-		love.graphics.draw(love.graphics.newImage("images/objects/voiture.png"), player.x,player.y,0, gameRessources.scaleX, gameRessources.scaleY)
+		love.graphics.draw(gameRessources.toy.car.img, player.x,player.y,0, gameRessources.scaleX, gameRessources.scaleY)
 	end
 
 	if drawCond.gift == true then
-		love.graphics.draw(myGameRessources.gift.blue.img,player.x,player.y,0,gameRessources.scaleX,gameRessources.scaleY)
+		love.graphics.draw(gameRessources.gift.blue.img,player.x,player.y,0,gameRessources.scaleX,gameRessources.scaleY)
 	end		
 
 end
